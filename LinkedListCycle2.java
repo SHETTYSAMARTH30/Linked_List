a5baf01856d0d9da187bf32f5a068ca0e57e5c0e
SHETTYSAMARTH30 <54606449+SHETTYSAMARTH30@users.noreply.github.com>
2020-06-26 22:32:55 -0700

    Create LinkedListCycle2.java

---
+++ LinkedListCycle2.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycle2 {
+    
+    public ListNode getIntersection(ListNode head){
+        
+        ListNode tortoise = head;
+        ListNode hare = head;
+        
+        while(hare != null && hare.next != null){
+            
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            
+            if(tortoise == hare)
+                return tortoise;
+        }
+        
+        return null;
+        
+    }
+    public ListNode detectCycle(ListNode head) {
+        
+        if(head == null)
+            return null;
+        
+        ListNode intersect = getIntersection(head);
+        
+        if(intersect == null)
+            return null;
+        
+        ListNode ptr1 = head;
+        ListNode ptr2 = intersect;
+        
+        while(ptr1 != ptr2){
+            
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        
+        return ptr1;
+        
+    }
+}
